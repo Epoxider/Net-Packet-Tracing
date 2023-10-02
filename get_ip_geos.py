122e95b3ce60a78a5e2d342df2db3e0afee2be23
Epoxide <matthew.r.fritz@gmail.com>
2023-10-02 14:49:23 -0700

    Added distance in miles logging

--- get_ip_geos.py
+++ get_ip_geos.py
@@ -1,10 +1,9 @@
-import requests, json, subprocess, re, sys, math
+import requests, json, subprocess, re, os, sys, math
 
-# add the path of this file to the system path
-sys.path.append('./tracert_proj')
+path = os.path.dirname(__file__)
 
 # read token from token.json
-with open('./tracert_proj/token.json', 'r') as f:
+with open(path+'/token.json', 'r') as f:
     token = json.load(f)['token']
 
 # Function to fetch geolocation data for a given IP address
@@ -46,6 +45,22 @@ def haversine_distance(origin, destination):
 
 # Function to perform a traceroute to a given IP destination
 def tracert(ip_dest):
+    """
+    Performs a traceroute to a given IP destination and returns the response times for each IP address along the route.
+
+    Parameters:
+        ip_dest (str): The IP address or domain name to perform the traceroute to.
+
+    Returns:
+        ip_ms_dict (dict): A dictionary containing the response times for each IP address along the route. The
+        dictionary contains the IP addresses as keys and a dictionary with the average response time in milliseconds
+        as the value. The dictionary has the following format:
+            {
+                'ip_address_1': {'avg_ms': response_time_1},
+                'ip_address_2': {'avg_ms': response_time_2},
+                ...
+            }
+    """
     ipv6_pattern = r"\d+\s+\d+\sms\s+(?P<time2>\d+)\sms\s+\d+\sms\s+(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|([0-9a-fA-F]{0,4}:){1,7}[0-9a-fA-F]{0,4}"
     cmd = f"tracert -d {ip_dest}"
     output = subprocess.check_output(cmd, shell=True).decode('utf-8')
@@ -61,16 +76,35 @@ def tracert(ip_dest):
 
     return ip_ms_dict
 
-def main():
-    ip_ms_data = tracert('8.8.8.8')
+def main(destination):
+    prev_cords = None
+    ip_ms_data = tracert(destination)
 
     for ip, ms_data in ip_ms_data.items():
         result = ip_geo(ip)
+
+        # Get latitude and longitude for the current IP address
+        try:
+            current_coords = tuple(map(float, result['loc'].split(','))) if 'loc' in result else None
+        except ValueError:
+            current_coords = None
+        
+        # If both previous_coords and current_coords are available, calculate the haversine distance
+        if prev_cords and current_coords:
+            distance = haversine_distance(prev_cords, current_coords)
+            result['distance_miles'] = distance * 0.621371  # Convert km to miles
+        
+        # Update previous_coords for the next iteration
+        prev_cords = current_coords
+
         result.update(ms_data)  # Add avg_ms to the result dictionary
         result['ip'] = ip  # Add the IP address to the result dictionary
         print(result)
 
-
 # Main program execution starts here
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) > 1:
+        destination = sys.argv[1]
+    else:
+        destination = 'youtube.com' # or ipv4/6 address
+    main(destination)
