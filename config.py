ac7ac5e607a3bbc678f16da2fe4a07bc3de1d050
Epoxide <matthew.r.fritz@gmail.com>
2023-10-05 12:08:39 -0700

    added changing line colors to map

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ class Config():
         self.km_to_miles = 0.621371
         self.earth_radius_km = 6371
         self.short_dest_name = re.sub(r'\W+', '_', self.destination.split('.')[0]) if any(char.isalpha() for char in self.destination) else self.destination
-        self.csv_path = os.path.join(os.path.dirname(__file__), f'/RouteData{self.short_dest_name}_route.csv')
-        self.map_path = os.path.join(os.path.dirname(__file__), f'/Maps/{self.short_dest_name}_map.html')
+        self.csv_path = os.path.join(os.path.dirname(__file__), f'{self.short_dest_name}_route.csv')
+        self.map_path = os.path.join(os.path.dirname(__file__), f'{self.short_dest_name}_map.html')
 
     def set_destination(self, destination: str) -> None:
         self.destination = destination
--- folium_map.py
+++ folium_map.py
@@ -1,3 +1,5 @@
+import colorsys
+import math
 from folium import Map, Marker, PolyLine
 from typing import Tuple, Optional
 
@@ -5,8 +7,8 @@ class FoliumMap():
     def __init__(self):
         self.folium_map = Map(location=[0, 0], zoom_start=3)
         self.line_coords = []
-        self.marker_counter = 0
         self.prev_coords: Optional[Tuple[float, float]] = None
+        self.min_hue_step = 0.05
     
     def update_prev_coords(self, current_coords):
         self.prev_coords = current_coords
@@ -20,13 +22,16 @@ class FoliumMap():
     def save_map(self, map_path):
         self.folium_map.save(map_path)
 
-    def add_line(self, current_coords, q_item):
-        # change color of line based on avg_ms of q_item, green for < 10ms, yellow for < 20ms, red for > 30ms
-        if q_item['avg_ms'] and int(q_item['avg_ms']) < 20:
-            PolyLine([current_coords[-2], current_coords[-1]], color="green", weight=2.5).add_to(self.folium_map)
-        elif q_item['avg_ms'] and int(q_item['avg_ms']) < 30:
-            PolyLine([current_coords[-2], current_coords[-1]], color="yellow", weight=2.5).add_to(self.folium_map)
-        elif q_item['avg_ms'] and int(q_item['avg_ms']) > 40:
-            PolyLine([current_coords[-2], current_coords[-1]], color="red", weight=2.5).add_to(self.folium_map)
-        else:
-            PolyLine([current_coords[-2], current_coords[-1]], color="blue", weight=2.5).add_to(self.folium_map)
+    def add_line(self, current_coords):
+        # change color of line based on number of lines already drawn
+        line_color = self.calc_color_hue(len(self.line_coords))
+        PolyLine([current_coords[-2], current_coords[-1]], color=line_color, weight=2.5).add_to(self.folium_map)
+
+    def calc_color_hue(self, line_count):
+        #hue = line_count / 40 # 
+        hue = self.min_hue_step * line_count
+        # Convert to RGB
+        r, g, b = colorsys.hsv_to_rgb(hue, 1, 1)
+        # Convert to HEX
+        color_hex = "#{:02x}{:02x}{:02x}".format(int(r * 255), int(g * 255), int(b * 255))
+        return color_hex
